2841a6a85069a85e9bae715f2c8ddf1e9f12a8ef
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2021-02-03 11:30:05 -0300

    lucky two

--- index.js
+++ index.js
@@ -5,26 +5,31 @@ const clear = require("clear");
 const figlet = require("figlet");
 const files = require("./lib/files");
 const inquirer = require("./lib/inquirer");
-const fetch = require('node-fetch');
-const currencyFormatter = require('currency-formatter');
+const fetch = require("node-fetch");
+const currencyFormatter = require("currency-formatter");
 
 clear();
 
 console.log(
-  chalk.yellow(figlet.textSync("BTConverter CLI", { horizontalLayout: "full" })),
-  'Love u Day sz'
-  );
-  
-  const run = async () => {
+  chalk.yellow(
+    figlet.textSync("BTConverter CLI", { horizontalLayout: "full" })
+  ),
+  "Love u Day sz"
+);
+
+const run = async () => {
   let value = await inquirer.askGithubCredentials();
 
-  const response = await fetch('https://brasilbitcoin.com.br/API/prices/BTC');
+  const response = await fetch("https://brasilbitcoin.com.br/API/prices/BTC");
   const responseObj = await response.json();
 
-  value = `${value.value}`.replace(/,/g, '');
+  value = `${value.value}`.replace(/,/g, "");
 
-  const totalPrice = currencyFormatter.format(Number(value) * responseObj.last, { code: 'BRL' });
-  console.log('Total price: ' + totalPrice);
+  const totalPrice = currencyFormatter.format(
+    Number(value) * responseObj.last,
+    { code: "BRL" }
+  );
+  console.log("Total price: " + totalPrice);
 };
 
 run();
