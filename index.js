3ec3eeac9572e55196ea6e4ad567e04213735994
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2021-02-03 10:41:42 -0300

    push library

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const files = require("./lib/files");
+const inquirer = require("./lib/inquirer");
+const fetch = require('node-fetch');
+const currencyFormatter = require('currency-formatter');
+
+clear();
+
+console.log(
+  chalk.yellow(figlet.textSync("BTConverter CLI", { horizontalLayout: "full" }))
+);
+
+if (files.directoryExists(".git")) {
+  console.log(chalk.red("Already a Git repository!"));
+  process.exit();
+}
+
+const run = async () => {
+  const value = await inquirer.askGithubCredentials();
+
+  const response = await fetch('https://brasilbitcoin.com.br/API/prices/BTC');
+  const responseObj = await response.json();
+
+  const totalPrice = currencyFormatter.format(value.value * responseObj.last, { code: 'BRL' });
+  console.log('Total price: ' + totalPrice);
+};
+
+run();
